5d9205bfe0ad756139e6d13b576f2f720c1f0abf
Matt O'Leary <matto2@users.noreply.github.com>
2025-06-05 06:55:06 -0700

    first

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mattoleary.design",
+  integrations: [tailwind(), icon(), sitemap()],
+
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+import { withAnimations } from 'animated-tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default withAnimations({
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-top-left': 'radial-gradient(circle at top left, #FFD6E8, transparent)',
+        'gradient-top-right': 'radial-gradient(circle at top right, #D6EFFF, transparent)',
+        'gradient-bottom-left': 'radial-gradient(circle at bottom left, #FFECD6, transparent)',
+        'gradient-bottom-right': 'radial-gradient(circle at bottom right, #D6FFD9, transparent)',
+      },
+      fontFamily: {
+        sans: ['"Onest"', 'sans-serif'], // Use Onest as the primary font
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const newUtilities = {
+        '.font-sans': {
+          fontFamily: theme('fontFamily.sans'),
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+});
